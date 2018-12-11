bf236804c700061e0a8ebce008e434cf06aaa68c
Oriol <oriolninr@gmail.com>
2018-12-11 16:07:45 +0100

    First commit

---
+++ quinusuari.php
@@ -0,0 +1,11 @@
+<?php
+$usuari = &$_POST["usuari"];
+echo "L'usuari es" . $usuari;
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
