c4d90564880939760266c23c1b0e022e37ea739f
jrubiralta <jordiruby@gmail.com>
2018-12-12 22:09:42 +0100

    Creació d'un alumne

--- registreAlumnes.php
+++ registreAlumnes.php
@@ -1,10 +1,9 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+session_start();
+if(!isset($_SESSION["username"])){
+    header("Location: http://localhost:8888/login.html", true, 301);
+    
+}
 
 $nom = $_GET["nom"];
 $cognoms = $_GET["cognoms"];
@@ -15,8 +14,8 @@ $contra = $_GET["pass2"];
 
 
 if($pass == $contra){
-    $servername = "127.0.0.1:1234";
-    $password = "1234";
+    $servername = "127.0.0.1:3306";
+    $password = "Jordirubi10!";
     $username = "root";
     $dbname = "projphp";
     $conn = new mysqli($servername, $username , $password, $dbname);
@@ -24,14 +23,14 @@ if($pass == $contra){
         die("Connection error " . $conn->connect_error);
     }
     else{
-       $sql = "INSERT INTO alumnes(nom, cognoms, usuari, pass, correu)"
-               . "VALUES ('". $nom . "', '" . $cognoms ."', '" . $user . "', '" . $pass. "', '" . $correu . "')";
+       $sql = "INSERT INTO alumnes(username, Password, Nom, Cognoms, Email)"
+               . "VALUES ('" . $user . "','" . $pass . "','" . $nom . "','" . $cognoms . "','" . $correu . "')";
        if(mysqli_query($conn, $sql)){
            echo "<h1>Alumne registrat amb exit</h1><br><br><a href=\"menuProfessors.php\">Torna al menu</a><br><br>";
-           echo"<a href=\"crearAlumne.html\">Registrar un altre alumne</a>";
+           echo "<a href=\"crearAlumne.html\">Registrar un altre alumne</a>";
        }
        else{
-           echo"S'ha produit un error " . mysqli_error($conn);
+           echo "S'ha produit un error " . mysqli_error($conn);
        }
     }
 }
